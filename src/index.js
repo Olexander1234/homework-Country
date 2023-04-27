86e16576896abd2d48e9828db7a45635b7c928f1
Olexander <cmurtimur@gmail.com>
2023-04-27 16:46:11 +0300

    папрь

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import debounce from 'lodash.debounce'
+
 const inputEl = document.querySelector('.input')
+const listEl = document.querySelector('.list')
 
-inputEl.addEventListener('input', debounce(onSearch, 500) )
-function onSearch (event){
-    event.preventDefault()
+inputEl.addEventListener('input', debounce(onSearch, 1000))
 
-    console.log(event.target.value);
-    const countryName = event.target.value
+function onSearch(event) {
+  event.preventDefault()
 
+  console.log(event.target.value)
+  const countryName = event.target.value
 
-    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+  fetch(`https://restcountries.com/v3.1/name/${countryName}`)
     .then(response => response.json())
-    .then(data=>console.log)
-    
+    .then(data => {
+      console.log(data)
+      // Використовуйте отримані дані для відображення розмітки
+      listEl.innerHTML = `
+        <h1>${data[0].name.common}</h1>
+        <p>Capital: ${data[0].capital}</p>
+        <p>Population: ${data[0].population}</p>
+        <p>Region: ${data[0].region}</p>
+      `
+      if (listEl.childElementCount != 0 && event.target.value == 0)  {
+        console.log(видаляєм );
+      } 
+      
+})
+
 }
